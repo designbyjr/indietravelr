2af32bbf65a8ab8c077e4a84365923d24a5c755e
designbyjr <coolmate108@hotmail.co.uk>
2017-09-14 20:47:18 +0100

    added menu
    added menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Hundreds of interviews with travel hackers who share their passion behind traveling and explain how they got to where they are today.">
+	<link rel="canonical" href="https://www.indietravelr.com">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="css/home.css">
+	<title>IndieTravelr - A travel hacking community</title>
+</head>
+<body>
+	@extends('layouts.title-block')
+</body>
+</html>
---
+++ resources/views/layouts/title-block.blade.php
@@ -0,0 +1,51 @@
+<div class="title-bar">
+		<div class="title-bar-content">
+			<a href="" class="logo-link">
+				<img src="LOGO.svg" alt="indie traveller logo" id="logo-box">
+				<img src="INDIE-TRAVELR.svg" alt="indie traveller logo name" id="logo">
+				<img src="R.svg" alt="indie traveller logo name" id="logotwo">
+			</a>
+			<ul class="nav-items-horizontal">
+				<li class="nav-item nav-hackers">
+					<a href="" class="nav-link">Travel Hackers</a>
+				</li>
+				<li class="nav-item nav-dest">
+					<a href="" class="nav-link">Destinations</a>
+				</li>
+				<li class="nav-item nav-tools">
+					<a href="" class="nav-link">Tools Etc</a>
+				</li>
+				<li class="nav-item nav-community">
+					<a href="" class="nav-link">Community</a>
+				</li>
+				<li class="nav-item nav-contribute">
+					<a href="" class="nav-link">Contribute</a>
+				</li>
+				<li class="nav-item nav-book">
+					<a href="" class="nav-link">Book Now</a>
+				</li>
+			</ul>
+			<ul class="nav-items-dropdown">
+				<li class="nav-item nav-hackers">
+					<a href="" class="nav-link">Travel Hackers</a>
+				</li>
+				<li class="nav-item nav-dest">
+					<a href="" class="nav-link">Destinations</a>
+				</li>
+				<li class="nav-item nav-tools">
+					<a href="" class="nav-link">Tools Etc</a>
+				</li>
+				<li class="nav-item nav-community">
+					<a href="" class="nav-link">Community</a>
+				</li>
+				<li class="nav-item nav-contribute">
+					<a href="" class="nav-link">Contribute</a>
+				</li>
+			</ul>
+			<div class="nav-btns">
+				<!-- <button class="book-now-btn"><span class="book-now">BOOK NOW</span></button> -->
+				<button class="book-now-btn"><span class="book-now">LOG IN</span></button>
+				<button class="book-now-btn">SIGN UP</button>
+			</div>
+		</div>
+	</div>
