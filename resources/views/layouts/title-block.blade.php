f53566e6866811145e283b927fcba983ef6a97f5
designbyjr <coolmate108@hotmail.co.uk>
2017-09-15 14:45:20 +0100

    responsive menu
    making menu responsive

--- resources/views/layouts/title-block.blade.php
+++ resources/views/layouts/title-block.blade.php
@@ -3,45 +3,55 @@
 			<a href="" class="logo-link">
 				<img src="LOGO.svg" alt="indie traveller logo" id="logo-box">
 				<img src="INDIE-TRAVELR.svg" alt="indie traveller logo name" id="logo">
-				<img src="R.svg" alt="indie traveller logo name" id="logotwo">
 			</a>
-			<ul class="nav-items-horizontal">
-				<li class="nav-item nav-hackers">
-					<a href="" class="nav-link">Travel Hackers</a>
-				</li>
-				<li class="nav-item nav-dest">
-					<a href="" class="nav-link">Destinations</a>
-				</li>
-				<li class="nav-item nav-tools">
-					<a href="" class="nav-link">Tools Etc</a>
-				</li>
-				<li class="nav-item nav-community">
-					<a href="" class="nav-link">Community</a>
-				</li>
-				<li class="nav-item nav-contribute">
-					<a href="" class="nav-link">Contribute</a>
-				</li>
-				<li class="nav-item nav-book">
-					<a href="" class="nav-link">Book Now</a>
-				</li>
-			</ul>
-			<ul class="nav-items-dropdown">
-				<li class="nav-item nav-hackers">
-					<a href="" class="nav-link">Travel Hackers</a>
-				</li>
-				<li class="nav-item nav-dest">
-					<a href="" class="nav-link">Destinations</a>
-				</li>
-				<li class="nav-item nav-tools">
-					<a href="" class="nav-link">Tools Etc</a>
-				</li>
-				<li class="nav-item nav-community">
-					<a href="" class="nav-link">Community</a>
-				</li>
-				<li class="nav-item nav-contribute">
-					<a href="" class="nav-link">Contribute</a>
-				</li>
-			</ul>
+			<div>
+				<ul class="nav-items-horizontal">
+					<li class="nav-item nav-hackers">
+						<a href="" class="nav-link">Travel Hackers</a>
+					</li>
+					<li class="nav-item nav-dest">
+						<a href="" class="nav-link">Destinations</a>
+					</li>
+					<li class="nav-item nav-tools">
+						<a href="" class="nav-link">Tools Etc</a>
+					</li>
+					<li class="nav-item nav-community">
+						<a href="" class="nav-link">Community</a>
+					</li>
+					<li class="nav-item nav-contribute">
+						<a href="" class="nav-link">Contribute</a>
+					</li>
+					<li class="nav-item nav-book">
+						<a href="" class="nav-link">Book Now</a>
+					</li>
+					<li class="nav-item nav-more">
+						More<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-expand-arrow"><path d="M12 21l-12-18h24z"></path>
+					</svg></li>
+					<li class="nav-item nav-menu">
+							<img src="menu.svg" alt="dropdown menu">
+					</li>
+				</ul>
+				<ul class="nav-items-dropdown">
+					<li class="nav-item nav-hackers">
+						<a href="" class="nav-link">Travel Hackers</a>
+					</li>
+					<li class="nav-item nav-dest">
+						<a href="" class="nav-link">Destinations</a>
+					</li>
+					<li class="nav-item nav-tools">
+						<a href="" class="nav-link">Tools Etc</a>
+					</li>
+					<li class="nav-item nav-community">
+						<a href="" class="nav-link">Community</a>
+					</li>
+					<li class="nav-item nav-contribute">
+						<a href="" class="nav-link">Contribute</a>
+					</li>
+					<li class="nav-item nav-book">
+						<a href="" class="nav-link">Book Now</a>
+					</li>
+				</ul>
+			</div>
 			<div class="nav-btns">
 				<!-- <button class="book-now-btn"><span class="book-now">BOOK NOW</span></button> -->
 				<button class="book-now-btn"><span class="book-now">LOG IN</span></button>
