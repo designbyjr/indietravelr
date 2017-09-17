bbaa2e1bc94704fca5f1eaf398f7b91f2571f076
designbyjr <coolmate108@hotmail.co.uk>
2017-09-17 16:31:20 +0100

    changed css,forms, background
    added forms, added background

--- resources/views/layouts/herodiv.blade.php
+++ resources/views/layouts/herodiv.blade.php
@@ -1,4 +1,17 @@
 <div class="hero-div">
 	<img src="THB.svg" alt="" class="thb">
-	<img src="thb.jpg" alt="" class="thbhidden">
+	<img src="thb.png" alt="" class="thbhidden">
 </div>
+<section class="home-intro">
+	<div class="home-introbox">
+		<h2>SIGN UP FOR TIPS & TRICKS FROM THE BEST HACKERS</h2>
+		<form action="">
+			<input type="text" name="first">
+			<input type="text" name="surname">
+			<select name="country" id="">
+				<option value=""></option>
+			</select>
+			<input type="submit" name="Get Travel Tips">
+		</form>
+	</div>
+</section>
