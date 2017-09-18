bbbc0e7dabaecd6f14364fbaa56e045264cc152a
designbyjr <coolmate108@hotmail.co.uk>
2017-09-18 11:49:26 +0100

    css change
    css change

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,1 @@
-'use strict';
-$(document).ready(function(){
-var dropdown = false;
-
-	$(".nav-more, .nav-menu").click(function(){
-		if(dropdown == false){
-			$('.nav-items-dropdown').css('display','flex');
-			dropdown = true;
-		}
-		else{
-			$('.nav-items-dropdown').css('display','none');
-			dropdown = false;
-		}
-
-		console.log('hello');
-	})
-
-});
+"use strict";$(document).ready(function(){var n=!1;$(".nav-more, .nav-menu").click(function(){0==n?($(".nav-items-dropdown").css("display","flex"),n=!0):($(".nav-items-dropdown").css("display","none"),n=!1)})});
